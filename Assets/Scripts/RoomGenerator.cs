b6c0cb5c992b265243eb71c4664a14e0091b1e13
Jingjia76 <113512173+Jingjia76@users.noreply.github.com>
2024-05-03 04:41:40 +0800

    最遠房間定義
    修復錯字
    foreach 所有房間內使用sqrMagnitude比對離初始房間最遠距離。

--- Assets/Scripts/RoomGenerator.cs
+++ Assets/Scripts/RoomGenerator.cs
@@ -19,6 +19,10 @@ public class RoomGenerator : MonoBehaviour
     public int roomNumber;
     //顏色有關區別
     public Color startColor,endColor;
+    //最後的房間
+    private GameObject endRoom;
+    
+
 
     [Header("位置控制")]
     //Point
@@ -26,9 +30,11 @@ public class RoomGenerator : MonoBehaviour
     //位移變量
     public float xOffset;
     public float yOffset;
+    //顯示要檢測的標籤
+    public LayerMask roomLayer;
 
     //生成列表保存產生的關卡
-    public List<GameObject> room = new List<GameObject>();
+    public List<GameObject> rooms = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
@@ -36,11 +42,28 @@ public class RoomGenerator : MonoBehaviour
         //生成房間
         for(int i = 0; i < roomNumber; i++) {
             //生成，且不旋轉
-            room.Add(Instantiate(roomPrefab,generatorPoint.position,Quaternion.identity));
+            rooms.Add(Instantiate(roomPrefab,generatorPoint.position,Quaternion.identity));
             //改變位置
             ChangePointPos();
         }
-        
+
+        //TODO:檢測房間_設定終點最遠房間距離
+        //初始房間顏色改變
+        rooms[0].GetComponent<SpriteRenderer>().color = startColor;    
+
+        //預設為初始房間，這樣在比較時越比越大，離初始房間越遠
+        endRoom = rooms[0];
+        //檢測每個房間
+        foreach (var room in rooms)
+        {
+            //比較目標點和原始點距離 sqrMagnitude是一種比較方式 ()
+            if(room.transform.position.sqrMagnitude > endRoom.transform.position.sqrMagnitude) 
+            {
+                endRoom = room;
+            }
+        }
+        //最終房間顏色改變
+        endRoom.GetComponent<SpriteRenderer>().color = endColor;
     }
 
     // Update is called once per frame
@@ -49,30 +72,32 @@ public class RoomGenerator : MonoBehaviour
         
     }
 
-    //改變位置
+    //TODO:改變位置
     public void ChangePointPos()
     {
-        //利用枚舉+switch製作位移
-        direction = (Direction)Random.Range(0,4);
-        // Debug.Log(":"+direction);
+        do{
+            //利用枚舉+switch製作位移
+            direction = (Direction)Random.Range(0,4);
 
-        switch(direction)
-        {
-            case Direction.up:
-                generatorPoint.position += new Vector3(0,yOffset,0);
-                break;
-            case Direction.down:
-                generatorPoint.position += new Vector3(0,-yOffset,0);
-                break;
-            case Direction.left:
-                generatorPoint.position += new Vector3(xOffset,0,0);
-                break;
-            case Direction.right:
-                generatorPoint.position += new Vector3(-xOffset,0,0);
-                break;
-            default:
-                Debug.Log("錯誤");
-                break;
-        }
+            switch(direction)
+            {
+                case Direction.up:
+                    generatorPoint.position += new Vector3(0,yOffset,0);
+                    break;
+                case Direction.down:
+                    generatorPoint.position += new Vector3(0,-yOffset,0);
+                    break;
+                case Direction.left:
+                    generatorPoint.position += new Vector3(xOffset,0,0);
+                    break;
+                case Direction.right:
+                    generatorPoint.position += new Vector3(-xOffset,0,0);
+                    break;
+                default:
+                    Debug.Log("錯誤");
+                    break;
+            }
+        }while(Physics2D.OverlapCircle(generatorPoint.position,0.2f,roomLayer));
+        //後執行
     }
 }
